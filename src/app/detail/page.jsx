f71f6b8d8a290befa2883c061eec3e5d2a205711
Maggie Muriithi <muriithinyawira13@gmail.com>
2023-12-04 11:16:13 +0300

    fixed router

--- src/app/detail/page.jsx
+++ src/app/detail/page.jsx
@@ -1,9 +1,10 @@
+"use client"
 import { useRouter } from "next/navigation";
 
-export const metadata = {
+/* export const metadata = {
   title: "Product",
   description: "Our Products",
-};
+}; */
 
 const ProductDetail = () => {
   const navigation = useRouter();
